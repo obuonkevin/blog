13aca923f1d4671f3e46488c81c6826df9bd924f
obuonkevin <75452687+obuonkevin@users.noreply.github.com>
2020-12-22 12:05:56 +0300

    Add files via upload

---
+++ controllers/users.js
@@ -0,0 +1,7 @@
+module.exports.controller = (app) => {
+  //get users page
+    app.get('/users', (req,res) => {
+  res.render ('users', {title:'Users',description:'This is the description of all the users',
+  love:'Love is the most amazing thing in the world. To love is to live'});
+})
+}
